afe3a10245868bca3666fb2103b82cefea21eb7c
Andrei Roh <andrei_roh@tut.by>
2020-10-20 15:26:38 +0300

    feat: update index.html
    Added a loader that appears on page load

---
+++ loader.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">$(window).on('load', function () {
+  let $preloader = $('#loader'),
+      $svg_anm   = $preloader.find('.svg_anm');
+  $svg_anm.fadeOut();
+  $preloader.delay(1000).fadeOut('slow');
+});</script>
