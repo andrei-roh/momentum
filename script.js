d8095e04827d87dd697ff696dd06fe7ab8a3d911
Andrei Roh <andrei_roh@tut.by>
2020-10-20 15:47:15 +0300

    Refactor: update index.html
    Added 25px width fo city div

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ if (localStorage.getItem('city') != 'Minsk') {
 }
 
 async function getWeather() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=en&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=en&appid=bffd331380de75029aefc8b85884024b&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
 
